aeb71a421f1009f9f998b6d2621a932149c5abfc
gweindel <gabriel.weindel@gmail.com>
2024-08-08 18:15:28 +0200

    Updated after submission of the revised version

--- README.md
+++ README.md
@@ -7,26 +7,15 @@
 ![Figure of the paper in 9 panels summarizing the HMP method, see caption of Figure 1 in the paper](plots/method.png)
 This repository contains all the data and code to reproduce the analysis and figures presented in the paper.
 
-- Folder plots/ contains the figure of the paper, a python script to reproduce the method plot (method_plot.py) and some GIMP (.xcf) and inkscape (.svg) files used for post-code figure editing
+- Folder plots/ contains the figure of the method and eventual plots produced wuth the scripts in results/
 - Folder results/simulations contains the data and code to reproduce the section "Simulations"
-- Folder results/replication contains the data and code to reproduce the section "Example application: HMP and decision-making"
+- Folder results/replication contains the data and code to reproduce the section "Application to real data"
 
 In order to reproduce the analysis and figures the easiest way is to install a specific conda environment as follows:
 
 ```bash
-conda create -n hmp pymc arviz xarray pandas mne bambi seaborn numpyro watermark
+conda create -n hmp pymc arviz xarray pandas mne bambi seaborn
 conda activate hmp
 conda install pip #if not already installed
-pip install hmp
+pip install hmp==0.4.0
 ```
-
-In case of problems, for an exact replication of the environment on which those analysis were done install the anaconda environment based on the .yml file
-```bash
-conda env create -f environment.yml
-```
-
-Note: Because of size constraints, the content of:
-- /results/replication/estimation_files/*
-- /results/simulations/simulated/*
-
-is zipped and stored on https://osf.io/29tgr/. The content needs to be unzipped and placed in the appropriate folders in order to run the code, note though that this is not obligatory but recreating these files through the scripts might take quite some time
---
+++ results/simulations/noiseless_illustration_section3-2/prop55.py
@@ -0,0 +1,42 @@
+import simulations
+## Importing these packages is specific for this simulation case
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import gamma
+
+## Importing HMP
+import hmp
+
+cpus = 1 # For multiprocessing, usually a good idea to use multiple CPUs as long as you have enough RAM
+
+n_trials = 1000 #Number of trials to simulate
+sfreq = 1000
+##### Here we define the sources of the brain activity (event) for each trial
+n_events = 4
+frequency = 10. #Frequency of the event defining its duration, half-sine of 10Hz = 50ms
+amplitude = 1 #Amplitude of the event in nAm, defining signal to noise ratio
+shape = 2 #shape of the gamma distribution
+
+names = ['superiorparietal-lh','inferiortemporal-lh','postcentral-lh','postcentral-rh','superiorparietal-lh']#Which source to activate for each event (see atlas when calling simulations.available_sources())
+name_file = 'dataset_prop55'
+
+
+means = np.array([50, 100, 250, 200,  150])/shape #Mean duration of the between event times in ms
+relations = [1,2,3,4,5] # to which previous event each event is related (1 is stimulus onset)
+proportions = [1,1,.55,1,1] # How frequent each event is
+event_length_samples = [50, 50, 50, 50, 50] # The sampling period of the event (e.g. 100 is full sine)
+
+
+sources = []
+for source in zip(names, means): #One source = one frequency, one amplitude and a given by-trial variability distribution
+    sources.append([source[0], frequency,  amplitude, gamma(shape, scale=source[1])])
+
+# Function used to generate the data
+file = simulations.simulate(sources, n_trials, cpus, name_file, path='data/',
+            overwrite=False, sfreq=sfreq, noise=False, seed=1234,
+            relations=relations, proportions=proportions, 
+            event_length_samples=event_length_samples)
+
+#load electrode position, specific to the simulations
+positions = simulations.simulation_positions()
